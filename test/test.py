2e95470f2a376db72546f1aee6d16219f9986cfb
komeilparseh <ahmdparsh129@gmail.com>
2021-05-14 22:29:43 +0430

    🧪 Add new test to file

--- test/test.py
+++ test/test.py
@@ -1,12 +1,20 @@
-from Sansorchi import sansor
+from Sansorchi import sansor, data
 
-txts = ["تو موزی موز", "خنگ خدای موز"]
-outputs = ["تو موزی م**ز", "خ**گ خدای م**ز"]
+txts = ["تو موزی موز", "خنگ خدا", "خر به پیش", "حیوانی وحشی"]
+outputs = ["تو موزی م**ز", "خ**گ خدا", "خ**ر به پیش", "ح**ی وحشی"]
 
 
-def test_answer():
-    """Sansorchi library test"""
+def test_sentences():
     i = 0
     for txt in txts:
         i += 1
         assert sansor(txt) == outputs[i-1]
+
+
+def test_word():
+    words = data.Json["word"]
+    for word in words:
+        # TODO: Suport MultiPart words.The problem is from the split[init.py:9]
+        if " " in word:
+            continue
+        assert sansor(word) == word[0]+"**"+word[-1]
