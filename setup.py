88a50e60fcbc72da2faf6e4c0188398e3de1e615
komeilparseh <ahmdparsh129@gmail.com>
2021-05-14 20:28:42 +0430

    ✨ Create setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Sansorchi',
     packages=['Sansorchi'],
-    version='1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    version='1.0.0',
     description='Sansorchi is a program to remove bad and inappropriate words',
     author='KomeilParseh',
     author_email='ahmdparsh129@gmail.com',
