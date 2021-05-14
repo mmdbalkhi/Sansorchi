2a843165f57b864d12729635bc5d57e02e7da696
komeilparseh <ahmdparsh129@gmail.com>
2021-05-14 20:42:09 +0430

    🔧 Build a test by pytest

--- test/test.py
+++ test/test.py
@@ -1,16 +1,10 @@
-from sansorchi import sansor
+from Sansorchi import sansor
 
 txts = ["تو موزی موز", "خنگ خدای موز"]
 outputs = ["تو موزی م**ز", "خ**گ خدای م**ز"]
 
-sucs = 0
-i = 0
-for txt in txts:
-    i += 1
-    if sansor(txt) == outputs[i-1]:
-        sucs += 1
-        print("\u001b[32m"+f"test {i}"+"\u001b[0m")
-    else:
-        print("\u001b[31m"+f"test {i}"+"\u001b[0m")
-
-print(f"{sucs} test(s) is successfully")
+def test_answer():
+    i = 0
+    for txt in txts:
+        i += 1
+        assert sansor(txt) == outputs[i-1]
