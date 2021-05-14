0b21236175e4759fbd17249adc5f92e61fced4fc
komeilparseh <ahmdparsh129@gmail.com>
2021-05-14 22:28:41 +0430

    🐛 Add Bug Description

--- Sansorchi/__init__.py
+++ Sansorchi/__init__.py
@@ -6,7 +6,9 @@ Json = Json["word"]
 def sansor(txt: str, bad=Json, **kwargs) -> str:
     """ This function takes an obligatory input called
     >>> txt and deletes it if there is an insult in it."""
-    split = txt.split(" ")
+    split = txt.split(
+        " ")  # BUG: This creates bug line, Separate words from space.
+    # >>>>>>>>> Multi-part words are separated
     a = ''
     for i in split:
         if i in bad:
