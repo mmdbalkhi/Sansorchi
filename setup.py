dfdd78fbbb3e05ad2825d81875a2856e74821a02
komeilparseh <ahmdparsh129@gmail.com>
2021-05-14 21:33:42 +0430

    ✨ Add some details

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='Sansorchi',
     packages=['Sansorchi'],
     version='1.0.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     description='Sansorchi is a program to remove bad and inappropriate words',
     author='KomeilParseh',
     author_email='ahmdparsh129@gmail.com',
     url='https://github.com/KomeilParseh/Sansorchi',
     download_url='https://github.com/KomeilParseh/Sansorchi',
     keywords=['Improper words', 'swearing', "sansorchi", "Sansorchi"],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GPL-3)',
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
