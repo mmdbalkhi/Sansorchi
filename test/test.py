97d32d5e1c000f9734ecb71c7ed549d1038aa35d
Linter <ahmdparsh129@gmail.com>
2021-07-04 18:06:24 +0000

    lint with Isort

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from Sansorchi import sansor, data
+from Sansorchi import data, sansor
 
 txts = ["تو موزی موز", "خنگ خدا", "خر به پیش", "حیوانی وحشی"]
 outputs = ["تو موزی م**ز", "خ**گ خدا", "خ**ر به پیش", "ح**ی وحشی"]
