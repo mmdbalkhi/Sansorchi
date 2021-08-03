601f171f00faaf1571037fdbf0bf2c1a1a75454a
komeilparseh <ahmdparsh129@gmail.com>
2021-08-03 12:47:00 +0430

    install auto:(

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-os.system('pip install "Sansorchi[docs]"')
+os.system('pip install -U myst_parser sphinx')
 
 # -- Project information -----------------------------------------------------
 
