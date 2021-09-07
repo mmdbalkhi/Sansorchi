0d37f105992a735513a26924fb4dddb6818d4cf0
mmdbalkhi <ahmdparsh129@gmail.com>
2021-09-07 15:21:37 +0430

    fix tests

--- test/test_word.py
+++ test/test_word.py
@@ -6,4 +6,5 @@ def test_word():
     """Unit test, Clean the code with the help of pylint"""
     swears = data.fa["word"]
     for swear in swears:
+        print(swear)
         assert sansor(swear) == str(len(swear) * "*")
